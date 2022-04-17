abcb587b571bedc510c08b026056f7d4b76dff94
littleLane <lzz857183384@163.com>
2022-04-17 22:06:19 +0800

    chapter3 debug

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Author: qianzhi
+ * @Date: 2022-04-17 21:54:02
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-17 21:59:09
+ * @FilePath: /vue-hackernews/src/components/Item.vue
+-->
 <template>
-  <div>
-    item
-  </div>
+  <div>item1</div>
 </template>
--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: qianzhi
+ * @Date: 2022-04-17 21:56:32
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-17 22:04:51
+ * @FilePath: /vue-hackernews/src/components/__tests__/Item.spec.js
+ */
 import { shallowMount } from '@vue/test-utils'
 import Item from '../Item.vue'
 
@@ -5,5 +12,11 @@ describe('Item.vue', () => {
   test('renders item', () => {
     const wrapper = shallowMount(Item)
     expect(wrapper.text()).toContain('item')
+    expect(wrapper.isVisible()).toBeTruthy()
+  })
+
+  test('renders item with DIV', () => {
+    const wrapper = shallowMount(Item)
+    expect(wrapper.element.tagName).toBe('DIV')
   })
 })
