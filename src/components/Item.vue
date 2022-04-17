0b583666a876d4469bc5a80646ccc0d1467dcc1b
littleLane <lzz857183384@163.com>
2022-04-17 21:52:29 +0800

    vue project debug

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Author: qianzhi
+ * @Date: 2022-04-17 21:44:10
+ * @LastEditors: qianzhi
+ * @LastEditTime: 2022-04-17 21:48:12
+ * @FilePath: /vue-hackernews/src/components/Item.vue
+-->
 <template>
   <div>
-    item
+    item1
   </div>
 </template>
